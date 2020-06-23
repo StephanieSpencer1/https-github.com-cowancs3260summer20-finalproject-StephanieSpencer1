708cc710464a8e308fc2cf67c0c1c9784974ece3
Stephanie Spencer <Stephanie.Spencer.22@hotmail.com>
2020-06-23 13:27:14 -0600

    Initial Commit

---
+++ Inventory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Inventory
+//
+//  Created by Nicholas Spencer on 6/23/20.
+//  Copyright © 2020 Stephanie Spencer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
