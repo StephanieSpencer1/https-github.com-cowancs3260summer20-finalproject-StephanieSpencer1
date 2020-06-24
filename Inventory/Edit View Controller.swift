e572a3b26db02529153f52b224151821bfdc1acc
Stephanie Spencer <Stephanie.Spencer.22@hotmail.com>
2020-06-23 21:12:45 -0600

    LAYOUT DONE

---
+++ Inventory/Edit View Controller.swift
@@ -0,0 +1,13 @@
+//
+//  Edit View Controller.swift
+//  Inventory
+//
+//  Created by Nicholas Spencer on 6/23/20.
+//  Copyright © 2020 Stephanie Spencer. All rights reserved.
+//
+
+import UIKit
+
+class Edit_View_Controller: NSObject {
+
+}
