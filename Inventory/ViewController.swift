fa3e75ac7fe3eccb399884eb539660cdf763b6fd
Stephanie Spencer <Stephanie.Spencer.22@hotmail.com>
2020-07-09 14:01:24 -0600

    final

--- Inventory/ViewController.swift
+++ Inventory/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import SQLite3
+
 
 struct Item {
     var shortDescription: String = ""
@@ -19,7 +21,8 @@ struct Item {
 var items: [Item] = []
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, AddItemProtocol, EditItemProtocol {
- 
+    
+    var db: OpaquePointer?
   
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
@@ -84,9 +87,64 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         self.navigationItem.title = "Inventory"
         // Do any additional setup after loading the view.
-       
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(saveToDatabase(_:)), name: UIApplication.willResignActiveNotification, object: nil)
+        let fileUrl = try!
+            FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("Inventory.sqlite")
+        if sqlite3_open(fileUrl.path, &db) != SQLITE_OK{
+            print("Error opening database")
+            return
+        }
+        let createTableQuery = "CREATE TABLE IF NOT EXISTS Items (id INTEGER PRIMARY KEY AUTOINCREMENT, shortDescription VARCHAR, longDescription VARCHAR)"
+        
+        if sqlite3_exec(db, createTableQuery, nil, nil, nil) != SQLITE_OK{
+            print("Error creating table")
+            return
+        }
+        //retrieve each item, initilize item object and append itme to items array
+        items.removeAll()
+        let selectQuery = "SELECT * FROM Items"
+        var stmt:OpaquePointer?
+        if sqlite3_prepare(db, selectQuery, -1, &stmt, nil) != SQLITE_OK {
+            print("error preparing insert")
+        }
+        while(sqlite3_step(stmt) == SQLITE_ROW){
+            let short = String(cString: sqlite3_column_text(stmt, 1))
+            let long = String(cString: sqlite3_column_text(stmt, 2))
+            items.append(Item.init(shortDescription: short, longDescription: long))
+        }
+                
     }
-    
+    @objc func saveToDatabase(_ notification:Notification){
+        //open DB??
+      
+        //execute delete from items to clear db
+        
+        //for each itme in array intert into db
+        for item in items{
+            //call prepare
+            let shortDescription = item.shortDescription
+            let longDescription = item.longDescription
+            var stmt: OpaquePointer?
+            let insertQuery = "INSERT INTO Items (shortDescription, longDescription) Values (?,?)"
+            if sqlite3_prepare(db, insertQuery, -1, &stmt, nil) != SQLITE_OK{
+                print("Error binding query")
+            }
+            //call bind
+            if sqlite3_bind_text(stmt, 1, (shortDescription as NSString).utf8String, -1, nil) != SQLITE_OK {
+                print("error binding shortDescription")
+            }
+            if sqlite3_bind_text(stmt, 2, (longDescription as NSString).utf8String, -1, nil) != SQLITE_OK {
+                           print("error binding shortDescription")
+                       }
+            //call step
+            sqlite3_step(stmt)
+        }
+            
+        //close db
+        sqlite3_close(db)
+
+    }
 
 
 
